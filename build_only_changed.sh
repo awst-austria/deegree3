99cd3887c9947ff7d54584c2f3888fe8be42343c
D. Baum <baum@awst.at>
2016-08-24 15:26:39 +0200

    Adding workaround that let's us host builds of changed libs on github

---
+++ build_only_changed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#mvn clean compile -pl \!deegree-spring,\!deegree-workspaces,\!deegree-themes,\!deegree-datastores,\!deegree-misc,\!deegree-layers,\!deegree-client,\!deegree-processproviders,\!deegree-tools,\!org.deegree:deegree-webservices -DskipTests
+
+mvn clean deploy -DskipTests -pl org.deegree:deegree-services-wps,org.deegree:deegree-protocol-csw
