f85c822ca6f4518a11c6bce9d447b077500fea8e
D. Baum <baum@awst.at>
2016-09-03 12:17:41 +0200

    Adding changed module to exclusive build script

--- build_only_changed.sh
+++ build_only_changed.sh
@@ -2,4 +2,4 @@
 
 #mvn clean compile -pl \!deegree-spring,\!deegree-workspaces,\!deegree-themes,\!deegree-datastores,\!deegree-misc,\!deegree-layers,\!deegree-client,\!deegree-processproviders,\!deegree-tools,\!org.deegree:deegree-webservices -DskipTests
 
-mvn clean deploy -DskipTests -pl org.deegree:deegree-services-wps,org.deegree:deegree-protocol-csw
+mvn clean deploy -DskipTests -pl org.deegree:deegree-services-wps,org.deegree:deegree-protocol-csw,org.deegree:deegree-protocol-commons
