168c8760f98fdd4b566cf45faff577355b76622d
D. Baum <baum@awst.at>
2016-09-03 12:27:31 +0200

    Removing unused module from exclusive build script

--- build_only_changed.sh
+++ build_only_changed.sh
@@ -2,4 +2,5 @@
 
 #mvn clean compile -pl \!deegree-spring,\!deegree-workspaces,\!deegree-themes,\!deegree-datastores,\!deegree-misc,\!deegree-layers,\!deegree-client,\!deegree-processproviders,\!deegree-tools,\!org.deegree:deegree-webservices -DskipTests
 
-mvn clean deploy -DskipTests -pl org.deegree:deegree-services-wps,org.deegree:deegree-protocol-csw,org.deegree:deegree-protocol-commons
+mvn clean deploy -DskipTests -pl org.deegree:deegree-protocol-csw,org.deegree:deegree-protocol-commons
+#org.deegree:deegree-services-wps,
